ee7903e6f0bc8f0a82b09c2577f20f1b042c5148
dragonstrategy <88288282+dragonstrategy@users.noreply.github.com>
2025-10-21 15:14:47 -0300

    Update vite.config.js

--- tienda-jruiz/vite.config.js
+++ tienda-jruiz/vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tienda-jruiz/',   // nombre de TU repo
+  base: '/tienda-jruiz/',
   build: { outDir: 'dist' }
 })
